9328b383afe65d3e3554eb86a6f05dd1c014b215
LunarJim <jim.deghaye@gmail.com>
2019-05-16 10:54:33 +0200

    homepage domaine JDE avec lien vers sites démo, cv et coordonnées

---
+++ projets.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css"">
+
+    <title>Projets-J. DEGHAYE</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row main align-items-center">
+      <div class="col-sm menu">
+        <div class="accordion" id="accordionExample">
+          <div class="card">
+            <div class="card-header" id="headingOne">
+              <h2 class="mb-0">
+              <button class="btn btn-link collapsed text-uppercase" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Le monde se divise en deux
+              </button>
+              </h2>
+            </div>
+
+          <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        Un concept simple ! "Le monde se divise en deux" : proposer une citation à chaud pour intégrer la communauté des amateurs de bons mots. Site réalisé sous WordPress avec thème custom, custom post type, plugin de gestion, compteur de likes (js Ajax, PDO, table custom), gestion de connexion, de soumission de posts, affichage par catégories, ratings, auteurs... Intégration bootstrap / Sass. (Responsive)
+        <br>
+        <a href="http://www.jeremie-deghaye.fr/le-monde-se-divise-en-deux" target="_blank"><button type="button" class="btn btn-info mt-2">Découvrir</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed text-uppercase" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Ma liste de courses
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+        Une liste de courses + todo utilisant PDO pour la connexion SQL. Intégration bootstrap, icônes fontawesome. Script JS (validation de saisie). (Responsive)
+        <br>
+        <a href="http://www.jeremie-deghaye.fr/liste-courses-demo" target="_blank"><button type="button" class="btn btn-info mt-2">Découvrir</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed text-uppercase" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Carlusso Autos
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        Un site sous WordPress, thème custom, custom post type, custom fields et différents plugins utilisables pour la vente de véhicules d'occasion. Intégration bootstrap avec galerie photo, carousel...(Responsive)
+        <br>
+        <a href="http://www.jeremie-deghaye.fr/carlusso" target="_blank"><button type="button" class="btn btn-info mt-2">Découvrir</button></a>
+        <a href="https://drive.google.com/file/d/1Tl5IpRGRx0F5cZlNPDdrYUhEfoT2BW0Q/view" target="_blank"><button type="button" class="btn btn-info mt-2">Démo video</button></a>
+      </div>
+    </div>
+
+    <div class="card">
+    <div class="card-header" id="headingFour">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed text-uppercase" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          JDMC : site "One page"
+        </button>
+      </h2>
+    </div>
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+      <div class="card-body">
+        Un site de présentation d'une entreprise de conseil avec plusieurs sections utilisant jQuery Rellax et Scrollex pour une navigation agréable et esthétique. Intégration Sass (Responsive).
+        <br>
+        <a href="http://www.jeremie-deghaye.fr/jdmc" target="_blank"><button type="button" class="btn btn-info mt-2">Découvrir</button></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+            </div>
+            <div class="col-sm JD text-center">
+                Projets
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
